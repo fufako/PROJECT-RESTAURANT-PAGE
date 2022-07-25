285daa949604a034465f2915b68c5d17ebdde483
fufako <patryko.mikiciuk@gmail.com>
2022-07-25 17:35:28 +0200

    Add loaders and HTML plugin

--- load.bundle.js
+++ load.bundle.js
@@ -1,2 +1,20 @@
-(()=>{const e=document.getElementById("content");console.log(e)})();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibG9hZC5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ik1BQUEsTUFBTUEsRUFBVUMsU0FBU0MsZUFBZSxXQUN4Q0MsUUFBUUMsSUFBSUosRSIsInNvdXJjZXMiOlsid2VicGFjazovL3Byb2plY3QtcmVzdGF1cmFudC1wYWdlLy4vc3JjL2ZpcnN0TG9hZC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBjb250ZW50ID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJjb250ZW50XCIpXG5jb25zb2xlLmxvZyhjb250ZW50KVxuIl0sIm5hbWVzIjpbImNvbnRlbnQiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwiY29uc29sZSIsImxvZyJdLCJzb3VyY2VSb290IjoiIn0=
+(self["webpackChunkproject_restaurant_page"] = self["webpackChunkproject_restaurant_page"] || []).push([["load"],{
+
+/***/ "./src/firstLoad.js":
+/*!**************************!*\
+  !*** ./src/firstLoad.js ***!
+  \**************************/
+/***/ (() => {
+
+const content = document.getElementById("content")
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/firstLoad.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibG9hZC5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Byb2plY3QtcmVzdGF1cmFudC1wYWdlLy4vc3JjL2ZpcnN0TG9hZC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBjb250ZW50ID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJjb250ZW50XCIpXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
